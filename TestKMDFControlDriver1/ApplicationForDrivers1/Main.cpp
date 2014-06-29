dc8f2c2fe2cee88cbfcde22cbd72c2ffad96ca4e
hjs <hjs>
2014-06-29 16:31:33 +0800

    Add WDFSample

---
+++ TestKMDFControlDriver1/ApplicationForDrivers1/Main.cpp
@@ -0,0 +1,95 @@
+#define INITGUID
+
+#include <Windows.h>
+#include <stdio.h>
+#include <Setupapi.h>
+#include "../TestKMDFControlDriver1/Public.h"
+
+#pragma comment(lib, "Setupapi.lib")
+
+HANDLE GetDeviceViaInterface(GUID* pGuid, DWORD instance) {
+
+	HANDLE rv = NULL;
+	// Get handle to relevant device information set
+	PSP_INTERFACE_DEVICE_DETAIL_DATA ifDetail = NULL;
+	HDEVINFO info = SetupDiGetClassDevs(pGuid, NULL, NULL, DIGCF_PRESENT | DIGCF_INTERFACEDEVICE);
+
+	if (info == INVALID_HANDLE_VALUE) {
+
+		printf("No HDEVINFO available for this GUID\n");
+
+		return NULL;
+
+	}
+
+	// Get interface data for the requested instance
+
+	SP_INTERFACE_DEVICE_DATA ifdata;
+
+	ifdata.cbSize = sizeof(ifdata);
+
+	if (!SetupDiEnumDeviceInterfaces(info, NULL, pGuid, instance, &ifdata)) {
+
+		printf("No SP_INTERFACE_DEVICE_DATA available for this GUID instance \n");
+
+		goto Exit;
+
+	}
+
+	// Get size of symbolic link name
+
+	DWORD ReqLen;
+
+	SetupDiGetDeviceInterfaceDetail(info, &ifdata, NULL, 0, &ReqLen, NULL);
+
+	ifDetail = (PSP_INTERFACE_DEVICE_DETAIL_DATA)(new char[ReqLen]);
+
+	if (ifDetail == NULL) {
+
+		goto Exit;
+
+	}
+
+	// Get symbolic link name
+
+	ifDetail->cbSize = sizeof(SP_INTERFACE_DEVICE_DETAIL_DATA);
+
+	if (!SetupDiGetDeviceInterfaceDetail(info, &ifdata, ifDetail, ReqLen, NULL, NULL)) {
+
+		goto Exit;
+
+	}
+
+	printf("Symbolic link is %s\n", ifDetail->DevicePath);
+
+		// Open file
+
+	rv = CreateFile(ifDetail->DevicePath, GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+
+Exit:
+	if (ifDetail)
+		delete ifDetail;
+
+	SetupDiDestroyDeviceInfoList(info);
+
+	return rv;
+
+}
+
+#define	VERIFY(x)	if(!(x)) {printf("Error %s\n", #x);}
+
+int main()
+{
+	GUID guid = GUID_DEVINTERFACE_TestKMDFControlDriver1;
+
+	for (int i = 0;; i++)
+	{
+		HANDLE handle = GetDeviceViaInterface(&guid, i);
+		if (!handle)
+			break;
+		char inbuffer[10];
+		char outbuffer[5];
+		DWORD byteReturn = 0;
+		VERIFY(DeviceIoControl(handle, 0x1234, inbuffer, sizeof(inbuffer), outbuffer, sizeof(outbuffer), &byteReturn, 0));
+	}
+}
---
+++ TestKMDFControlDriver1/TestKMDFControlDriver1/Public.h
@@ -0,0 +1,24 @@
+﻿/*++
+
+Module Name:
+
+    public.h
+
+Abstract:
+
+    This module contains the common declarations shared by driver
+    and user applications.
+
+Environment:
+
+    user and kernel
+
+--*/
+
+//
+// Define an Interface Guid so that app can find the device and talk to it.
+//
+
+DEFINE_GUID (GUID_DEVINTERFACE_TestKMDFControlDriver1,
+    0xbe5a5076,0xc70d,0x4b01,0x94,0xea,0x00,0xd3,0x63,0x73,0x27,0x3e);
+// {be5a5076-c70d-4b01-94ea-00d36373273e}
